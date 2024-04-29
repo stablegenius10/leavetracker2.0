c3f304ec463d03926db3737bd29ea7f716e391c2
stablegenius10 <ayushgoswami3010@gmail.com>
2024-04-29 13:08:17 +0530

    done

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/leave-tracker")
+  .connect("mongodb+srv://ayush:ayush@cluster0.hkkuuyw.mongodb.net/")
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err));
 
